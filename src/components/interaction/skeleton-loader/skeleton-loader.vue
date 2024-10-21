55a417cac12f86c0c50c162a8a7d4cd4468e677b
Lewis Howles <lewishowles@users.noreply.github.com>
2024-10-21 12:46:21 +0100

    `skeleton-loader`: Enforce a single root element to make usage simpler

--- src/components/interaction/skeleton-loader/skeleton-loader.vue
+++ src/components/interaction/skeleton-loader/skeleton-loader.vue
@@ -1,9 +1,11 @@
 <template>
-	<div class="sr-only" aria-live="polite" data-test="skeleton-loader">
-		<slot name="label">
-			Loading…
-		</slot>
-	</div>
+	<div>
+		<div class="sr-only" aria-live="polite" data-test="skeleton-loader">
+			<slot name="label">
+				Loading…
+			</slot>
+		</div>
 
-	<slot />
+		<slot />
+	</div>
 </template>
