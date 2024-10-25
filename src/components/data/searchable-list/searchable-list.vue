3246355f5bb0e8b38e7e799a359f71787407b0ba
Lewis Howles <lewishowles@users.noreply.github.com>
2024-10-25 12:37:25 +0100

    `searchable-list`: Filter results based on provided search query

--- src/components/data/searchable-list/searchable-list.vue
+++ src/components/data/searchable-list/searchable-list.vue
@@ -1,19 +1,23 @@
 <template>
 	<div data-test="searchable-list">
 		<div class="mb-6 flex gap-4">
-			<form-input v-bind="{ placeholder }" class="w-full max-w-lg" data-test="searchable-list-search">
+			<form-input v-bind="{ placeholder }" v-model="searchQuery" class="w-full max-w-lg" data-test="searchable-list-search">
 				<slot name="label" />
 			</form-input>
 		</div>
 
 		<div data-test="searchable-list-results">
-			<slot v-bind="{ items: data }" />
+			<slot v-bind="{ items: results }" />
 		</div>
 	</div>
 </template>
 
 <script setup>
-defineProps({
+import { computed, ref } from "vue";
+import { isNonEmptyString } from "@lewishowles/helpers/string";
+import { objectContains } from "@lewishowles/helpers/object";
+
+const props = defineProps({
 	/**
 	 * The list of items to search, or display if no search is being performed.
 	 * This should be an array of objects. Any non-object entries will be
@@ -32,5 +36,19 @@ defineProps({
 		type: String,
 		default: null,
 	},
+});
+
+const searchQuery = ref("");
+// Whether a search query has been provided and a search can be performed. This
+// excludes whitespace at each end of the query.
+const haveSearchQuery = computed(() => isNonEmptyString(searchQuery.value, { trim: true }));
+
+// The items to display, based on any current search query.
+const results = computed(() => {
+	if (!haveSearchQuery.value) {
+		return props.data;
+	}
+
+	return props.data.filter(item => objectContains(item, searchQuery.value, { caseInsensitive: true, allowPartial: true }));
 });
 </script>
