4e322e25ca5ed1861981d79a1b0243181aab76dc
Lewis Howles <lewishowles@users.noreply.github.com>
2025-05-23 16:42:05 +0100

    Add documentation for `form-actions`

---
+++ src/views/components/forms/page-form-actions/fragments/playground-form-actions.vue
@@ -0,0 +1,53 @@
+<template>
+	<component-playground v-bind="{ copy: template }" id="playground-form-actions">
+		<template #title>
+			Form actions
+		</template>
+
+		<form-actions>
+			<ui-button type="submit" class="button--primary">
+				Create account
+			</ui-button>
+
+			<template #tertiary-actions>
+				<link-tag href="#">
+					Cancel
+				</link-tag>
+			</template>
+		</form-actions>
+	</component-playground>
+</template>
+
+<script setup>
+import useTemplateGenerator from "@/views/components/composables/use-template-generator/use-template-generator";
+
+const slots = {
+	"default": {
+		value: useTemplateGenerator("ui-button", {
+			props: {
+				type: {
+					value: "submit",
+					inline: true,
+				},
+				class: {
+					value: "button--primary",
+					inline: true,
+				},
+			},
+
+			slots: {
+				default: {
+					value: "Create account",
+				},
+			},
+
+			indent: 1,
+		}),
+	},
+	"tertiary-actions": {
+		value: "",
+	},
+};
+
+const template = useTemplateGenerator("form-actions", { slots });
+</script>
---
+++ src/views/components/forms/page-form-actions/page-form-actions.vue
@@ -0,0 +1,37 @@
+<template>
+	<component-page>
+		<template #title>
+			Form actions
+		</template>
+
+		<template #introduction>
+			<p><code>form-actions</code> is a functional component that adds consistent layout of the actions of a form.</p>
+		</template>
+
+		<component-slots>
+			<component-slot id="slot-default">
+				<template #name>
+					default
+				</template>
+
+				<p>The default slot contains the content to wrap.</p>
+			</component-slot>
+
+			<component-slot id="slot-tertiary-actions">
+				<template #name>
+					tertiary-actions
+				</template>
+
+				<p>Any additional actions that should appear below the main row.</p>
+			</component-slot>
+		</component-slots>
+
+		<component-playgrounds>
+			<playground-form-actions />
+		</component-playgrounds>
+	</component-page>
+</template>
+
+<script setup>
+import PlaygroundFormActions from "./fragments/playground-form-actions.vue";
+</script>
