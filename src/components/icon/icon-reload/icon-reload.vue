5300f9bc17fe21dfb3c5e8975dc626bca9fad183
Lewis Howles <lewishowles@users.noreply.github.com>
2025-06-05 13:05:16 +0100

    Tweak reload icon

--- src/components/icon/icon-reload/icon-reload.vue
+++ src/components/icon/icon-reload/icon-reload.vue
@@ -1,28 +1,30 @@
 <template>
 	<base-icon>
-		<g fill="none" fill-rule="evenodd">
-			<path
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="1.5"
-				d="M1.872 2.555 2.28 5.5l2.944-.407"
-			/>
-			<path
-				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="1.5"
-				d="M16.232 7.734A7.25 7.25 0 0 0 2.29 5.5"
-			/>
-			<circle cx="14.127" cy="13.377" r="1" fill="currentColor" fill-rule="nonzero" />
-			<circle cx="9" cy="15.5" r="1" fill="currentColor" fill-rule="nonzero" />
-			<circle cx="3.873" cy="13.377" r="1" fill="currentColor" fill-rule="nonzero" />
-			<circle cx="1.75" cy="8.25" r="1" fill="currentColor" fill-rule="nonzero" />
-			<circle cx="15.698" cy="11.024" r="1" fill="currentColor" fill-rule="nonzero" />
-			<circle cx="11.774" cy="14.948" r="1" fill="currentColor" fill-rule="nonzero" />
-			<circle cx="6.226" cy="14.948" r="1" fill="currentColor" fill-rule="nonzero" />
-			<circle cx="2.302" cy="11.024" r="1" fill="currentColor" fill-rule="nonzero" />
+		<path
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="1.5"
+			d="M1.872 2.555 2.28 5.5l2.944-.407"
+		/>
+		<path
+			fill="none"
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="1.5"
+			d="M16.232 7.734A7.25 7.25 0 0 0 2.29 5.5"
+		/>
+		<g fill="currentColor" stroke="none">
+			<circle cx="14.127" cy="13.377" r="1" />
+			<circle cx="9" cy="15.5" r="1" />
+			<circle cx="3.873" cy="13.377" r="1" />
+			<circle cx="1.75" cy="8.25" r="1" />
+			<circle cx="15.698" cy="11.024" r="1" />
+			<circle cx="11.774" cy="14.948" r="1" />
+			<circle cx="6.226" cy="14.948" r="1" />
+			<circle cx="2.302" cy="11.024" r="1" />
 		</g>
 	</base-icon>
 </template>
