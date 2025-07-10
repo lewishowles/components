d817def44291578569e665aeb1705cd1e80637d3
Lewis Howles <lewishowles@users.noreply.github.com>
2025-07-10 12:19:32 +0100

    Jazz up the icons list a little

--- src/views/components/icon/page-icons/page-icons.vue
+++ src/views/components/icon/page-icons/page-icons.vue
@@ -8,6 +8,8 @@
 			<p>A number of icons are available baked into the component library and globally available, which allows their use as strings via props and in other components. These icons are primarily from Nucleo and Phosphor Icons, and some have been tweaked to better suit the library.</p>
 
 			<p>Each icon is listed here in alphabetical order, and can be used as a regular component, e.g. <code>&lt;icon-arrow-up /&gt;</code></p>
+
+			<p>Most of the icons here were added out of necessity for this project or others, and more can be added as required.</p>
 		</template>
 
 		<searchable-list v-bind="{ data: icons }">
@@ -16,13 +18,13 @@
 			</template>
 
 			<template #default="{ items }">
-				<ol class="flex flex-col gap-4">
-					<li v-for="icon in items" :key="icon.name" class="flex items-center gap-4 text-sm group">
-						<component :is="icon.name" class="size-6 text-purple-800" />
+				<ol class="grid grid-cols-[repeat(auto-fit,minmax(13rem,1fr))] gap-4">
+					<li v-for="icon in items" :key="icon.name" class="flex flex-col items-center justify-center relative px-4 py-6 rounded-lg border border-grey-200 dark:border-transparent dark:bg-white/5">
+						<component :is="icon.name" class="size-6 text-purple-800 dark:text-purple-300 mb-4" />
 
-						<span class="font-mono">{{ icon.name }}</span>
+						<span class="font-mono text-center text-xs">{{ icon.name }}</span>
 
-						<copy-content v-bind="{ content: icon.name }" class="button--muted opacity-0 group-hover:opacity-100 focus-visible:opacity-100">
+						<copy-content v-bind="{ content: icon.name }" class="text-sm absolute justify-center rounded-lg bg-white dark:bg-purple-950/30 backdrop-blur-xl inset-0 opacity-0 group-hover:opacity-100 hover:opacity-100 focus-visible:opacity-100">
 							Copy icon name
 						</copy-content>
 					</li>
