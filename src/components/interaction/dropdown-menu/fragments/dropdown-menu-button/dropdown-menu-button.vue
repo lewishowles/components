73cb8758d88d6adeae7ddbe6979d5da46ac3a69f
Lewis Howles <lewishowles@users.noreply.github.com>
2025-11-17 15:55:43 +0000

    [Fix] Properly align content in `dropdown-menu-button`

--- src/components/interaction/dropdown-menu/fragments/dropdown-menu-button/dropdown-menu-button.vue
+++ src/components/interaction/dropdown-menu/fragments/dropdown-menu-button/dropdown-menu-button.vue
@@ -1,6 +1,6 @@
 <template>
 	<ui-button
-		class="w-full border-s-2 px-4 py-2 hocus:border-current hocus:bg-grey-50 hocus:text-purple-800 dark:hocus:bg-white/10 dark:hocus:text-purple-300"
+		class="w-full border-s-2 px-4 py-2 justify-start hocus:border-current hocus:bg-grey-50 hocus:text-purple-800 dark:hocus:bg-white/10 dark:hocus:text-purple-300"
 		:class="{
 			'border-current bg-grey-50 text-purple-800 dark:bg-white/10 dark:text-purple-300': selected,
 			'border-transparent': !selected,
