2a03f2d50099c1e03edc338b18cc93d2a2ae4db9
Lewis Howles <lewishowles@users.noreply.github.com>
2026-05-14 00:17:04 +0100

    Update homepage icons

--- src/docs/views/page-home/fragments/icon/icon-display.vue
+++ src/docs/views/page-home/fragments/icon/icon-display.vue
@@ -8,14 +8,12 @@
 				x="6"
 				y="9"
 				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-linejoin="round"
 				stroke-width="4"
 				rx="1.5"
 			/>
 			<circle cx="29.25" cy="18.75" r="2.25" fill="currentColor" />
-			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="m27.62 30.75 4.817-4.811a1.5 1.5 0 0 1 2.121 0L42 33.386" />
-			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="m6 31.63 10.189-10.191a1.5 1.5 0 0 1 2.122 0L35.871 39" />
+			<path stroke="currentColor" stroke-width="4" d="m27.62 30.75 4.817-4.811a1.5 1.5 0 0 1 2.121 0L42 33.386" />
+			<path stroke="currentColor" stroke-width="4" d="m6 31.63 10.189-10.191a1.5 1.5 0 0 1 2.122 0L35.871 39" />
 		</g>
 	</base-icon>
 </template>
