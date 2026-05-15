5b85e7a2dffc683aa4432d0b4394bcc057aeecd1
Lewis Howles <lewishowles@users.noreply.github.com>
2026-05-15 20:58:47 +0100

    fix(tests): Ensure default content

--- src/components/messaging/modal-dialog-title/modal-dialog-title.cy.js
+++ src/components/messaging/modal-dialog-title/modal-dialog-title.cy.js
@@ -1,7 +1,8 @@
 import ModalDialogTitle from "./modal-dialog-title.vue";
 import { createMount } from "@cypress/support/mount";
 
-const mount = createMount(ModalDialogTitle);
+const defaultSlots = { default: "Confirm action" };
+const mount = createMount(ModalDialogTitle, { slots: defaultSlots });
 
 describe("modal-dialog-title", () => {
 	it("A component is rendered", () => {
