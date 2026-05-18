990be5d1e166305511f0d4f97eb9a9630fde91d3
Lewis Howles <lewishowles@users.noreply.github.com>
2026-05-18 17:28:08 +0100

    docs(ui-button): strengthen the `loadingAuto` example to demonstrate a workflow

--- src/docs/views/components/interaction/page-ui-button/fragments/playground-ui-button-loading-auto.vue
+++ src/docs/views/components/interaction/page-ui-button/fragments/playground-ui-button-loading-auto.vue
@@ -5,12 +5,17 @@
 		</template>
 
 		<template #introduction>
-			<p>When <code>loadingAuto</code> is set, the button detects a Promise returned by the click handler and automatically enters its loading state, resetting when the Promise settles. Click the button below to see it in action.</p>
+			<p>When a button is <code>reactive</code>, its <code>loadingAuto</code> defaults to true. The button detects a Promise returned by the click handler and automatically enters its loading state, resetting when the Promise resolves or rejects. Click the button below to see it in action.</p>
 		</template>
 
-		<ui-button v-bind="componentProps" @click="simulateSave">
-			{{ textSlots.default?.value }}
-		</ui-button>
+		<div class="flex items-center gap-4">
+			<ui-button v-bind="componentProps" @click="simulateSave">
+				{{ textSlots.default?.value }}
+			</ui-button>
+
+			<span v-show="processing">Saving…</span>
+			<span v-show="saveComplete">Saved!</span>
+		</div>
 	</component-playground>
 </template>
 
@@ -28,11 +33,6 @@ const textSlots = ref({
 
 // Props both for the template and for the component example itself.
 const props = ref({
-	loadingAuto: {
-		label: "Loading auto",
-		value: true,
-		type: "boolean",
-	},
 	reactive: {
 		label: "Reactive",
 		value: true,
@@ -53,6 +53,12 @@ const events = ref({
 	},
 });
 
+// A processing flag to demonstrate when our async function is invoked.
+const processing = ref(false);
+// Mock a "save complete" state so that we can demonstrate a workflow to the
+// user.
+const saveComplete = ref(false);
+
 // Convert our props into a format that can be passed directly to our component.
 const componentProps = computed(() => {
 	return Object.fromEntries(
@@ -61,8 +67,14 @@ const componentProps = computed(() => {
 });
 
 // Simulate an async operation to demonstrate loadingAuto.
-function simulateSave() {
-	return new Promise(resolve => setTimeout(resolve, 2000));
+async function simulateSave() {
+	processing.value = true;
+	saveComplete.value = false;
+
+	await new Promise(resolve => setTimeout(resolve, 2000));
+
+	processing.value = false;
+	saveComplete.value = true;
 }
 
 const template = useTemplateGenerator("ui-button", { slots: textSlots, props, events });
