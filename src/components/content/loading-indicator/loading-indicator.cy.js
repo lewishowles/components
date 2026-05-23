e6e769be7227857026e3d351b614beb5acaa3b44
Lewis Howles <lewishowles@users.noreply.github.com>
2026-05-23 12:04:51 +0100

    test(loading-indicator): properly assert structure of loading-indicator

--- src/components/content/loading-indicator/loading-indicator.cy.js
+++ src/components/content/loading-indicator/loading-indicator.cy.js
@@ -1,12 +1,14 @@
 import LoadingIndicator from "./loading-indicator.vue";
 import { createMount } from "@cypress/support/mount";
 
-const mount = createMount(LoadingIndicator);
+const defaultSlots = { default: "Loading" };
+const mount = createMount(LoadingIndicator, { slots: defaultSlots });
 
 describe("loading-indicator", () => {
 	it("A component is rendered", () => {
 		mount();
 
 		cy.getByData("loading-indicator").shouldBeVisible();
+		cy.getByData("loading-indicator").shouldHaveText("Loading");
 	});
 });
