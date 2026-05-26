acaed13cd4b35961309babb521341b028ae92496
Lewis Howles <lewishowles@users.noreply.github.com>
2026-05-26 12:35:38 +0100

    feat(progress-bar): minor design tweak for standard progress

--- src/components/chart/progress-bar/progress-bar.vue
+++ src/components/chart/progress-bar/progress-bar.vue
@@ -130,7 +130,7 @@ const props = defineProps({
 	 */
 	trackClasses: {
 		type: [String, Array, Object],
-		default: "h-4 rounded-full bg-grey-200 dark:bg-white/20",
+		default: "h-4 p-1 rounded-full bg-grey-200 dark:bg-white/20",
 	},
 
 	/**
