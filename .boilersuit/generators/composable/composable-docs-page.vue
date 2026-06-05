a017513b860726678adbc7d395720e4ff9610577
Lewis Howles <lewishowles@users.noreply.github.com>
2026-06-05 12:33:13 +0100

    chore(boilersuit): add docs page to composable generator

---
+++ .boilersuit/generators/composable/composable-docs-page.vue
@@ -0,0 +1,41 @@
+<template>
+	<component-page>
+		<template #title> {{ NAME | pascal }} </template>
+
+		<template #introduction>
+			<p><code>{{ NAME | camel }}</code> [describe what this composable manages].</p>
+		</template>
+
+		<component-parameters>
+			<component-parameter id="parameter-example">
+				<template #name> example </template>
+
+				<template #type> string </template>
+
+				<template #default-value> null </template>
+
+				<p>[Describe this parameter.]</p>
+			</component-parameter>
+		</component-parameters>
+
+		<component-returns>
+			<component-return id="return-example">
+				<template #name> example </template>
+
+				<template #type> Ref&lt;boolean&gt; </template>
+
+				<p>[Describe this returned value.]</p>
+			</component-return>
+		</component-returns>
+
+		<component-methods>
+			<component-method id="method-example">
+				<template #name>
+					<code>example()</code>
+				</template>
+
+				<p>[Describe this method.]</p>
+			</component-method>
+		</component-methods>
+	</component-page>
+</template>
