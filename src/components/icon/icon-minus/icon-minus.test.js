7380f8604c1ef8893d138c8fd2180489f4df8ceb
Lewis Howles <lewishowles@users.noreply.github.com>
2026-09-23 12:26:21 +0100

    fix(icon-minus): fix copy-pasta

--- src/components/icon/icon-minus/icon-minus.test.js
+++ src/components/icon/icon-minus/icon-minus.test.js
@@ -1,10 +1,10 @@
 import { createMount } from "@lewishowles/testing/vue";
 import { describe, expect, test } from "vite-plus/test";
-import IconPlus from "./icon-plus.vue";
+import IconMinus from "./icon-minus.vue";
 
-const mount = createMount(IconPlus);
+const mount = createMount(IconMinus);
 
-describe("icon-plus", () => {
+describe("icon-minus", () => {
 	describe("Initialisation", () => {
 		test("should exist as a Vue component", () => {
 			const wrapper = mount();
